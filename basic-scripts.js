2fbd142a5259653c30609fb9ba31072baccdf3c1
Ali Shalabi <alishalabi@Alis-MacBook-Pro-2.local>
2019-01-08 19:51:01 -0800

    Added layout of homepage, added js

---
+++ basic-scripts.js
@@ -0,0 +1,8 @@
+function showAnswer() {
+  var x = document.getElementById("answer")
+  x.style.backgroundColor = "white";
+
+  var y = document.getElementById("fyi")
+  y.style.display = "block";
+  
+}
