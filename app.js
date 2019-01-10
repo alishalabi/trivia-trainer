7d12fdb1eacac45be004cebf1d511f34d50d967e
Ali Shalabi <alishalabi@Alis-MacBook-Pro-2.local>
2019-01-09 16:58:22 -0800

    Migrated basic html/css/js over to a node project (hbs for template)

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 const express = require("express")
-const exphbs = require('express-handlebars');
+const exphbs = require("express-handlebars")
+const http = require("http")
 
 const app = express()
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
+app.use(express.static('public'))
 
 app.get("/", (req, res) => {
-  res.render('home', { msg: 'Handlebars are Cool!' });
+  // console.log(req.query.term)
+  // let queryString = "maine";
+  // let term = encodeURIComponent(queryString)
+  // let url = "http://en.wikipedia.org/w/api.php?action=opensearch&search=" + term +"&limit=1&format=json"
+  //
+  // http.get(url, (response) => {
+  //   response.setEncoding("utf8")
+  //   // Question: how do I store response as body
+  //   let body = response;
+  //   let parsed = JSON.parse(body)
+  //   res.render("home", {fyi: parsed.data})
+  //   })
+  res.render("home")
 })
 
 app.listen(3000, () => {
