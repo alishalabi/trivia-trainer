374339ddbe3c51814aa2c00e171941c1c20591a1
Ali Shalabi <alishalabi@Alis-MacBook-Pro-2.local>
2019-01-10 16:37:21 -0800

    Got Wikipedia API to work, added light front-end (commented out), started working on Jeopardy API.

--- app.js
+++ app.js
@@ -7,10 +7,14 @@ const express = require("express")
 const exphbs = require("express-handlebars")
 const bodyParser = require("body-parser")
 const http = require("http")
+const axios = require("axios")
 const mongoose = require("mongoose")
 const cookieParser = require("cookie-parser")
 const jwt = require("jsonwebtoken")
+const jserviceNode = require("jservice-node")
+const js = require('jservice-node');
 
+// Instantiate express
 const app = express()
 
 // Integrating middleware
@@ -36,57 +40,37 @@ app.get("/", (req, res) => {
   let url = "http://en.wikipedia.org/w/api.php?action=opensearch&search="+term+"&limit=1&format=json"
 
 
-  // http.get(url, (resp) => {
-  //   let data = '';
-  //   console.log("Making the request " + resp)
-  //   // A chunk of data has been recieved.
-  //   resp.on('data', (chunk) => {
-  //     console.log("CHUNK DATA " + chunk)
-  //     data += chunk;
-  //   });
-  //
-  //   // The whole response has been received. Print out the result.
-  //   resp.on('end', () => {
-  //     console.log("Reached the end")
-  //     // console.log(JSON.parse(data).explanation);
-  //   });
-  //
-  // }).on("error", (err) => {
-  //   console.log("Error: " + err.message);
-  // });
+  // Axios Method
 
+  axios.get(url)
+  .then(response => {
+    // console.log(response.data[2])
+    // console.log(response.data.url);
+    // console.log(response.data.explanation);
+    res.render("home", {fyiDecript: response.data[2], fyiLink: response.data[3]})
+  })
+  .catch(error => {
+    console.log(error);
+  });
 
-//   http.get(url, (response) => {
-//     // response.setEncoding("utf8")
-//     // console.log(response)
-//     // Question: how do I store response as body
-//     // let body = response;
-//     // console.log(body)
-//     // FIXME: this is where this is breaking
-//     // Step 1: HTTP Call to get JSON
-//     // Step 2: Parse JSON into HTML
-//     // Step 3: Pass HTML into template
-//     const parsed = JSON.parse(body);
-//     // console.log(parsed)
-//     // response.on('data', (d) => {
-//     //   console.log('start')
-//     //   body += d;
-//     //   const parsed = JSON.parse(body);
-//     //   // console.log(parsed)
-//     // })
-//     //
-//     // response.on('end', () => {
-//     //   // const parsed = JSON.parse(body);
-//     //   console.log("ended")
-//     // })
-//     // let parsed = JSON.parse(body)
-//     // res.render("home", {fyi: parsed.data})
-//     })
-//   res.render("home")
-// })
-res.render("home")
 })
 
+const gameCategories = []
+app.get("/game/start", (req, res) => {
+    var catOptions = {
+      count: 5,
+      offset: 25
+  };
+
+  //
+  js.categories(catOptions, function(error, response, json){
+      if(!error && response.statusCode == 200){
+          res.render("start", {categories: json});
+      } else {
+          console.log(`Error: ${response.statusCode}`);
+      }
+  });
+})
 
 app.listen(3000, () => {
   console.log('App listening on port 3000!')
