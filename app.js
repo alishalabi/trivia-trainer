2d68211fd7fe21e858918ed108638a9dc125112b
Ali Shalabi <alishalabi@Alis-MacBook-Pro-2.local>
2019-01-14 12:57:47 -0800

    working on get one route

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/trivia-binge')
 // Connecting controller
 const auth = require("./controllers/auth.js")(app)
 
+// Render Homepage: Random Question
 app.get("/", (req, res) => {
 
   // Grab one random question from jservice-node
@@ -63,35 +64,58 @@ app.get("/", (req, res) => {
   });
 })
 
-app.get("/sample", (req, res) => {
-  js.random(9, function(error, response, json){
+// Render Homepage: Single Question by ID
+app.get("/a/:id", (req, res) => {
+  const reference = {
+    id: req.params.id,
+  }
+  js.clues(reference, function(error, response, json){
     if(!error && response.statusCode == 200){
-      const samples = json
-        res.render("sample", {samples: samples})
+        console.log(json[0].question);
+        const question = json[0].question;
+        const answer = json[0].answer;
+        const category = json[0].category.title;
+        let queryString = answer;
+        let term = encodeURIComponent(queryString)
+        let url = "http://en.wikipedia.org/w/api.php?action=opensearch&search="+term+"&limit=1&format=json"
+
+        // Populate Wiki entry using axios
+        axios.get(url)
+        .then(response => {
+          res.render("home", {
+            fyiDecript: response.data[2],
+            fyiLink: response.data[3],
+            question: question,
+            answer: answer,
+            category: category
+          })
+        })
+        .catch(error => {
+          console.log(error);
+        });
     } else {
         console.log(`Error: ${response.statusCode}`);
     }
-});
-
-})
-
-// Testing category generation
-const gameCategories = []
-app.get("/game/start", (req, res) => {
-    var catOptions = {
-      count: 5,
-      offset: 25
-  };
-
-  //
-  js.categories(catOptions, function(error, response, json){
-      if(!error && response.statusCode == 200){
-          res.render("start", {categories: json});
-      } else {
-          console.log(`Error: ${response.statusCode}`);
-      }
   });
 })
+
+// // Testing category generation
+// const gameCategories = []
+// app.get("/game/start", (req, res) => {
+//     var catOptions = {
+//       count: 5,
+//       offset: 25
+//   };
+//
+//   //
+//   js.categories(catOptions, function(error, response, json){
+//       if(!error && response.statusCode == 200){
+//           res.render("start", {categories: json});
+//       } else {
+//           console.log(`Error: ${response.statusCode}`);
+//       }
+//   });
+// })
 
 
 app.listen(process.env.PORT || 3000, (req, res) => {
